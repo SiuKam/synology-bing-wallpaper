438a99f71fb6951803f9052a45c6f772ba3a1505
Zhaozhuo Gan <41332305+SiuKam@users.noreply.github.com>
2023-04-20 16:38:06 +0800

    Update synology_bing_wallpaper.sh

--- synology_bing_wallpaper.sh
+++ synology_bing_wallpaper.sh
@@ -2,7 +2,7 @@
 savepath="/volume1/download/wallpaper"
 # using api provided by atlinker.cn
 pic_info=$(wget -t 5 --no-check-certificate -qO- "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
-pic_url=$(echo https://www.bing.com$(echo $pic_info|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g'))
+pic_url=$(echo https://www.cn.bing.com$(echo $pic_info|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g'))
 date=$(echo $pic_info|sed 's/.\+enddate[": ]\+//g'|grep -Eo 2[0-9]{7}|head -1)
 savefile=$savepath/$date"_bing.jpg"
 wget -t 5 --no-check-certificate $pic_url -qO $savefile
@@ -11,14 +11,12 @@ rm -rf /usr/syno/etc/login_background*.jpg
 cp -f $savefile /usr/syno/etc/login_background.jpg &>/dev/null
 cp -f $savefile /usr/syno/etc/login_background_hd.jpg &>/dev/null
 copyright=$(echo $pic_info|sed 's/.\+"copyright[:" ]\+//g'|sed 's/".\+//g')
-cninfo=$(echo $copyright|sed 's/，/"/g'|sed 's/,/"/g'|sed 's/(/"/g'|sed 's/ //g'|sed 's/\//_/g'|sed 's/)//g')
-line_1=$(echo $cninfo|cut -d'"' -f1)
-line_2=$(echo $cninfo|cut -d'"' -f2)
+title=$(echo $pic_info|sed 's/.\+"title[:" ]\+//g'|sed 's/".\+//g')
 sed -i s/login_background_customize=.*//g /etc/synoinfo.conf
 echo "login_background_customize=\"yes\"">>/etc/synoinfo.conf
 sed -i s/login_welcome_title=.*//g /etc/synoinfo.conf
-echo "login_welcome_title=\"$line_1\"">>/etc/synoinfo.conf
+echo "login_welcome_title=\"$title\"">>/etc/synoinfo.conf
 sed -i s/login_welcome_msg=.*//g /etc/synoinfo.conf
-echo "login_welcome_msg=\"$line_2\"">>/etc/synoinfo.conf
+echo "login_welcome_msg=\"$copyright\"">>/etc/synoinfo.conf
 # DO remember to change the username
 cp -f $savefile /usr/syno/etc/preference/gan/wallpaper
