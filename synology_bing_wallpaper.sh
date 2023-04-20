de9eb9e1b6366c23598f839c337b2a45a8374690
Zhaozhuo Gan <41332305+SiuKam@users.noreply.github.com>
2023-04-20 16:39:56 +0800

    Update synology_bing_wallpaper.sh

--- synology_bing_wallpaper.sh
+++ synology_bing_wallpaper.sh
@@ -1,6 +1,6 @@
 # saving location
 savepath="/volume1/download/wallpaper"
-# using api provided by atlinker.cn
+# using api provided by cn.bing.com
 pic_info=$(wget -t 5 --no-check-certificate -qO- "https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
 pic_url=$(echo https://www.cn.bing.com$(echo $pic_info|sed 's/.\+"url"[:" ]\+//g'|sed 's/".\+//g'))
 date=$(echo $pic_info|sed 's/.\+enddate[": ]\+//g'|grep -Eo 2[0-9]{7}|head -1)
